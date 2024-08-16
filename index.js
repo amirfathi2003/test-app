5076e35180d316d740cfdcebe6d86d4a0e2965b4
Amir Fathabadi <amirfathabadi2003@gmail.com>
2024-08-16 22:34:56 +0330

    add stars function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+function stars(rows) {
+  for (row = 1; row <= rows; row++) {
+    pattern = "";
+    for (i = 0; i < +row; i++) {
+      pattern += "*";
+    }
+  }
+  console.log(pattern);
+}
